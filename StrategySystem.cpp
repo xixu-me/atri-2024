0afee1e35787299f7b5a8b9393e62ed4df2542a5
Xi Xu <i@xi-xu.me>
2023-12-01 23:35:35 +0800

    Update StrategySystem.cpp

--- StrategySystem.cpp
+++ StrategySystem.cpp
@@ -2410,7 +2410,7 @@ void CStrategySystem::Goalie() {
 	static int xit;
 
 	if (ball.oldPosition.x < hgoalie.position.x && ball.position.x < hgoalie.position.x) {
-		int xix = ball.position.x >= 730 && (ball.position.y <= 361 || ball.position.y >= 457) || ball.position.x >= 940 ? 965 : 950;
+		int xix = ball.position.x >= 730 && (ball.position.y <= 313 || ball.position.y >= 505) || ball.position.x >= 873 && abs(ball.position.y - ball.oldPosition.y) > 10 || ball.position.x >= 940 ? 965 : 950;
 		int xiy = 409;
 
 		if (ball.position.x >= 600) {
